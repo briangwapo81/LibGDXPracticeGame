fd671eb3db586f4938395e61f93909bb001e59c4
Thunder <hahahaungguy@gmail.com>
2024-04-20 18:48:35 +0800

    Finished game -but with shit attached like:      -The score bar is messed up      -Font sucks ass...

--- core/src/com/mygdx/game/TestingGame.java
+++ core/src/com/mygdx/game/TestingGame.java
@@ -1,6 +1,5 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
@@ -8,7 +7,6 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
@@ -16,7 +14,6 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 
-import javax.swing.*;
 import java.util.Iterator;
 
 public class TestingGame implements Screen {
